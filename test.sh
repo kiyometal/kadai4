49ac483d2e4778d893e0a189e93d4c65ebef11f6
Sugawara <40025341+kiyometal@users.noreply.github.com>
2018-06-16 15:09:57 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+tmp=/tmp/$$
+
+ERROR_EXIT () {
+  echo $1 1>&2
+  rm -f $tmp-*
+  exit 1
+}
+
+# test1: 引数の数
+echo "input 2 argments" > $tmp-arg
+./18745125.sh 1> $tmp-ans && ERROR_EXIT "test1-1: エラー終了しない"
+diff $tmp-ans $tmp-arg || ERROR_EXIT "test1-2: エラーメッセージが違う"
+
+# test2: 自然数
+echo "input natural numbers" > $tmp-nat
+./18745125.sh a b 2> $tmp-ans && ERROR_EXIT "test2-1: エラー終了しない"
+diff $tmp-ans $tmp-nat || ERROR_EXIT "test2-2: エラーメッセージが違う"
+
+# test3: 一致
+echo "1 is equal to 1" > $tmp-same
+./18745125.sh 1 1 > $tmp-ans || ERROR_EXIT "test3-1: "
+diff $tmp-ans $tmp-same || ERROR_EXIT "test3-2"
+
+echo "0 is equal to 2" > $tmp-same
+./18745125.sh 0 2 > $tmp-ans || ERROR_EXIT "test3-3"
+diff $tmp-ans $tmp-same || ERROR_EXIT "test3-4"
+
+rm -f $tmp-*
+~              
